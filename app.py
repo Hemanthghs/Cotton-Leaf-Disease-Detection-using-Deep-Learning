a59da2a953caada6dd337ab2c6b0a0a1edfb00aa
Hemanthsai G <ghshemanth@gmail.com>
2021-07-02 20:34:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
 from flask import Flask, render_template, request
 import os
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
-from keras.models import load_model
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
 
 model = load_model("model.h5")
 
-def predict(file_name):
-    img = cv2.imread(file_name)
-    img = cv2.resize(img,(150,150))
-    img = img_to_array(img)/255
-    img = np.expand_dims(img,axis=0)
-    prediction = model.predict(img)
-    
-    prediction = prediction[0]
-    
-    return int(prediction >= 0.5)
+def predict(filepath):
+  img = plt.imread(filepath)
+  temp_img = img
+  img = cv2.resize(img,(150,150))
+  img = img_to_array(img)/255
+  img = np.expand_dims(img,axis=0)
+  prediction = model.predict(img) >= 0.5
+  if prediction==1:
+    prediction = "Fresh Cotton Leaf"
+    print("Prediction: "+prediction)
+    return 1
+  else:
+    prediction = "Diseased Cotton Leaf"
+    print("Prediction: "+prediction)
+    return 0
+  
+
     
 
 
